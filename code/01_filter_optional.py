253071a125f42b10498111bb1fea0d5199176dcc
Robert Alexander <gogonegro@gmail.com>
2024-02-20 18:49:27 +0100

    renamed

---
+++ code/01_filter_optional.py
@@ -0,0 +1,24 @@
+""" Reduce Kaggle's Wine Reviews data from 130K to around 20K objects selecting only Italian wines
+    Only keep the title as the ID of the wine and the description field
+    
+    NB as this file is too large to be hosted easily on Github please
+    download it from https://www.kaggle.com/datasets/christopheiv/winemagdata130k
+    and place it in the resources subdirectory
+"""
+
+import json
+
+with open("resources/winemag-data-130k-v2.json", "r", encoding="utf-8") as f:
+    wine_list_loaded = json.load(f)
+
+print(len(wine_list_loaded))
+
+filtered_wines = [
+    {"title": obj["title"], "description": obj["description"]}
+    for obj in wine_list_loaded
+    if obj["country"] == "Italy"
+]
+print(len(filtered_wines))
+
+with open("resources/italian-wines-20k-v2.json", "w", encoding="utf-8") as f:
+    json.dump(filtered_wines, f, ensure_ascii=False)
